45f880c8a9d1ff5cf63f4678b1bdb447c1ffcef8
Thore Strassburg <thore@strassburg.me>
2025-12-01 21:57:06 +0100

    feat(lsp): use native document color support
    The support for this feature was added in recent nightly versions.
    This requires to disable the `nvim-highlight-colors` plugin to avoid
    double color indicators. However, it is still relevant for file types
    without a language server that has support. Should be observed.

--- after/pack/plugins/opt/nvim-highlight-colors/plugin/setup.lua
+++ after/pack/plugins/opt/nvim-highlight-colors/plugin/setup.lua
@@ -4,4 +4,14 @@ require('nvim-highlight-colors').setup({
   virtual_symbol_position = 'inline',
   enable_named_colors = false,
   enable_tailwind = false,
+  -- Disable in advantage of native feature via LSP when available.
+  exclude_buffer = function(buffer)
+    local any_attached_client_provides_colors = vim
+      .iter(vim.lsp.get_clients({ bufnr = buffer }))
+      :find(function(client)
+        return client:supports_method('textDocument/documentColor')
+      end)
+
+    return any_attached_client_provides_colors
+  end,
 })
--- plugin/lsp/enable_features.lua
+++ plugin/lsp/enable_features.lua
@@ -1,2 +1,17 @@
 vim.lsp.on_type_formatting.enable()
 vim.lsp.linked_editing_range.enable()
+
+-- TODO: Why does this API work differently? All this just to change the `style`
+local group = vim.api.nvim_create_augroup('Lsp/enable_features', {})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = group,
+  desc = 'enable LSP document colors with non default style',
+  callback = function(arguments)
+    local client = vim.lsp.get_client_by_id(arguments.data.client_id)
+
+    if client ~= nil and client:supports_method('textDocument/documentColor') then
+      vim.lsp.document_color.enable(true, arguments.buf, { style = 'virtual' })
+    end
+  end,
+})
