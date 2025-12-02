2458b023dd344a9aad2c5a5400746e3b474e7a3c
Thore Strassburg <thore@strassburg.me>
2025-12-02 15:30:10 +0100

    chore(plugins/target.vim): remove obsolete plugin
    I feel like native text objects got better. In other cases, Treesitter
    and LSP based text-objects fill remaining gaps.

--- after/pack/plugins/start/targets.vim/plugin_specification.lua
+++
@@ -1,4 +0,0 @@
-require('packer').use({ 'wellle/targets.vim', opt = true })
-
--- Note: There are way too many operator mappings to lazy load this based on
--- mappings. Therefore the event must be efficient enough.
