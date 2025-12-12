eac70e0dcea3f211b8b25f4018f426b8bc391cce
Thore Strassburg <thore@strassburg.me>
2025-12-12 13:11:31 +0100

    fix(plugins/inc-rename.nvim): recover from errors
    The renaming via the plugins sometimes fails during rename preparation
    calls. That was annoying, as this required to trigger "normal" renaming
    differently. Now it is used as recovery case automatically.

--- after/pack/plugins/start/inc-rename.nvim/plugin/mappings.lua
+++ after/pack/plugins/start/inc-rename.nvim/plugin/mappings.lua
@@ -2,5 +2,9 @@ require('plugin_manager').lazy_load_plugin_on_command('inc-rename.nvim', 'IncRen
 
 vim.keymap.set('n', 'grN', function()
   local current_name = vim.fn.expand('<cword>')
-  vim.cmd.IncRename(current_name)
+  local _, error = pcall(vim.cmd.IncRename, current_name)
+
+  if error then
+    vim.lsp.buf.rename()
+  end
 end, { desc = 'rename target under cursor with preview' })
