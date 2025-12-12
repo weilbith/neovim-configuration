3df90a249d7dcae5495cd5fb24404992ee068e78
Thore Strassburg <thore@strassburg.me>
2025-12-12 13:10:20 +0100

    feat(plugins/hop.nvim): adapt highlights
    After recent colorscheme changes, the highlights were not working too
    well anymore.

--- after/pack/plugins/opt/hop.nvim/plugin/highlights.lua
+++ after/pack/plugins/opt/hop.nvim/plugin/highlights.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_set_hl(0, 'HopNextKey',{ link = 'Search' })
-vim.api.nvim_set_hl(0, 'HopNextKey1',{ link = 'Search' })
-vim.api.nvim_set_hl(0, 'HopNextKey2',{ link = 'IncSearch' })
+vim.api.nvim_set_hl(0, 'HopNextKey', { link = 'CurSearch' })
+vim.api.nvim_set_hl(0, 'HopNextKey1', { link = 'CurSearch' })
+vim.api.nvim_set_hl(0, 'HopNextKey2', { link = 'IncSearch' })
