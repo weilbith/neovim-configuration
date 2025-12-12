765ccc1fac94f786c26c5e2def023ab0cfe9aa90
Thore Strassburg <thore@strassburg.me>
2025-12-12 13:17:03 +0100

    chore(lsp/codelens): simplify auto command
    The check for client support is no more necessary. The native
    implementation got more robust and does not throw errors anymore.

--- plugin/lsp/codelens/autocommands.lua
+++ plugin/lsp/codelens/autocommands.lua
@@ -3,12 +3,6 @@ local group = vim.api.nvim_create_augroup('CodeLens', {})
 vim.api.nvim_create_autocmd({ 'LspAttach', 'InsertLeave' }, {
   group = group,
   callback = function(arguments)
-    local supporting_clients = vim.tbl_filter(function(client)
-      return client:supports_method('textDocument/codeLens')
-    end, vim.lsp.get_clients({ bufnr = arguments.buf }))
-
-    if #supporting_clients > 0 then
-      vim.lsp.codelens.refresh({ bufnr = arguments.buf })
-    end
+    vim.lsp.codelens.refresh({ bufnr = arguments.buf })
   end,
 })
