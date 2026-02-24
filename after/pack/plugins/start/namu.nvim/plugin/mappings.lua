079067bd942200e02a2760a0723fa94dbac7f99b
Thore Strassburg <thore@strassburg.me>
2026-02-24 10:24:55 +0100

    feat(plugins/namu.nvim): improve key bindings
    Natively, "gO" is used to open an outline. I tried to remain conform.
    Because "go" is used for moving by bytes. Something I never use.
    Therefore, I continue with the logic to use lower character for buffer
    local action and upper case for workspace global.

--- after/pack/plugins/start/namu.nvim/plugin/mappings.lua
+++ after/pack/plugins/start/namu.nvim/plugin/mappings.lua
@@ -1,5 +1,9 @@
 require('plugin_manager').lazy_load_plugin_on_command('namu.nvim', 'Namu')
 
-vim.keymap.set('n', 'gO', function()
+vim.keymap.set('n', 'go', function()
   vim.cmd.Namu('symbols')
-end, { desc = 'open filterable tree of symbols for navigation' })
+end, { desc = 'open filterable tree of symbols in current buffer' })
+
+vim.keymap.set('n', 'gO', function()
+  vim.cmd.Namu('workspace')
+end, { desc = 'open filterable tree of symbols in whole workspace' })
