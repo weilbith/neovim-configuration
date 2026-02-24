c64cad9bbf2ec83509eec0352e08d6f7a84b63f8
Thore Strassburg <thore@strassburg.me>
2026-02-24 11:20:21 +0100

    feat(diagnostics): show as virtual lines on jumps

--- plugin/diagnostics.lua
+++ plugin/diagnostics.lua
@@ -1,7 +1,16 @@
-local namespace = vim.api.nvim_create_namespace('lsp/diagnostic')
+local namespace_for_virtual_line_diagnostics = vim.api.nvim_create_namespace('lsp/diagnostic')
 
-local function number_to_circular_number(_, index)
-  return (circular_numbers[index] or index) .. ' '
+---@param diagnostic? vim.Diagnostic
+---@param buffer integer
+local function show_diagnostic_on_jump(diagnostic, buffer)
+  if diagnostic ~= nil then
+    vim.diagnostic.show(
+      namespace_for_virtual_line_diagnostics,
+      buffer,
+      { diagnostic },
+      { virtual_lines = { current_line = true }, virtual_text = false }
+    )
+  end
 end
 
 vim.diagnostic.config({
@@ -18,10 +27,22 @@ vim.diagnostic.config({
       [vim.diagnostic.severity.HINT] = '󰍢',
     },
   },
+  jump = { on_jump = show_diagnostic_on_jump },
   float = {
     border = 'single',
     header = '',
     anchor_bias = 'below',
     source = true,
   },
+})
+
+local group = vim.api.nvim_create_augroup('diagnostics', { clear = true })
+
+vim.api.nvim_create_autocmd('DiagnosticChanged', {
+  group = group,
+  callback = function(args)
+    -- TODO: Only hide, if diagnostics that have been shown changed.
+    vim.diagnostic.hide(namespace_for_virtual_line_diagnostics, args.buf)
+  end,
+  desc = 'Fix dangling virtual line diagnostics that have been shown, but diagnostics changed due to editing/fixing them',
 })
