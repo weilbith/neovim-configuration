efdcf59453e96736c7d20110dab7b3d0f2e2275d
Thore Strassburg <thore@strassburg.me>
2026-02-25 12:01:58 +0100

    feat(lsp/kotlin-lsp): add new client

---
+++ lsp/kotlin-lsp.lua
@@ -0,0 +1,12 @@
+---@type vim.lsp.Config
+return {
+  cmd = { 'kotlin-lsp', '--stdio' },
+  filetypes = { 'kotlin' },
+  root_markers = {
+    'settings.gradle',
+    'settings.gradle.kts',
+    'build.gradle',
+    'build.gradle.kts',
+    'workspace.json',
+  },
+}
