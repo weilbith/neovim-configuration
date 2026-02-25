816337bbb189564d2d0494d05b6f21793f7a428f
Thore Strassburg <thore@strassburg.me>
2026-02-25 11:51:46 +0100

    fix(plugins/dropbar.nvim): adapt to upstream changes

--- after/pack/plugins/opt/dropbar.nvim/lua/dropbar_custom/heirline.lua
+++ after/pack/plugins/opt/dropbar.nvim/lua/dropbar_custom/heirline.lua
@@ -3,7 +3,7 @@ local icons = require('icons')
 return {
   provider = function()
     ---@diagnostic disable-next-line: undefined-field
-    local content = _G.dropbar.get_dropbar_str()
+    local content = _G.dropbar()
     content = content:gsub('%%%*', '')
     content = content:gsub(icons.ChevronRight, '%%#WhiteBold#' .. icons.ChevronRight)
     return content
